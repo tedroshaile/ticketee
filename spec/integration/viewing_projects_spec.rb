3161c922a57bbb46f5af665bb4c964ca39cc2789
Ted Haile <theodore.haile@gmail.com>
2013-05-14 14:51:58 -0700

    Implement creating tickets for a project

--- spec/integration/viewing_projects_spec.rb
+++ spec/integration/viewing_projects_spec.rb
@@ -4,8 +4,7 @@ feature "Viewing projects" do
 	scenario "Listing all projects" do
 		project = Factory.create(:project, :name => "TextMate 2")
 		visit '/'
-		binding.pry
-    click_link 'TextMate 2'
+		click_link 'TextMate 2'
 		page.current_url.should == project_url(project)
 	end
 end
