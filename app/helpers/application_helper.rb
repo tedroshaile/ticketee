7a8ab662b98b2007f5e0d95b218881bc33db4ec5
Ted Haile <theodore.haile@gmail.com>
2013-05-15 14:54:09 -0700

    Lock down specific projecs controller actions for admins only

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,12 @@
 module ApplicationHelper
-	def title(*parts) 
+	def title(*parts)
 		unless parts.empty?
-			content_for :title do 
+			content_for :title do
 				(parts << "Ticketee").join(" - ")
 			end
 		end
 	end
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
 end
